3efaf1448f31576c2de9de099facc5c393ecaa9c
Yuliya <lomeyka@gmail.com>
2020-09-01 10:00:53 +0300

    container in header, began to create buttons in header

---
+++ webpack.config.js
@@ -0,0 +1,164 @@
+const path = require('path')
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+const {CleanWebpackPlugin} = require('clean-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
+const TerserWebpackPlugin = require('terser-webpack-plugin')
+
+
+const isDev = process.env.NODE_ENV === 'development'
+
+const isProd = !isDev
+
+const optimization = () => {
+  const config = {
+    splitChunks: {
+      chunks: 'all'
+    }
+  }
+  if (isProd) {
+    config.minimizer = [
+      new OptimizeCSSAssetsPlugin(),
+      new TerserWebpackPlugin(),
+    ]
+  }
+  return config
+
+}
+
+const cssLoaders = extra => {
+  const loaders = [{
+    loader: MiniCssExtractPlugin.loader,
+    options: {
+      hmr: isDev,
+      reloadAll: true,
+    },
+  },'css-loader']
+
+  if(extra) {
+    loaders.push(extra)
+  }
+  return loaders
+}
+
+const babelOptions = preset => {
+  const opts = {
+    presets: [
+      '@babel/preset-env'
+    ],
+    plugins: [
+      '@babel/plugin-proposal-class-properties'
+    ]
+  }
+
+  if(preset) {
+    opts.presets.push(preset)
+  }
+
+  return opts
+}
+
+const jsLoaders = () => {
+  const loaders = [{
+    loader: 'babel-loader',
+    options: babelOptions()
+  }]
+
+  if (isDev) {
+    loaders.push('eslint-loader')
+  }
+
+  return loaders
+}
+
+
+module.exports = {
+  context: path.resolve(__dirname, 'src'),
+  mode: "development",
+  entry: {
+    main: ['@babel/polyfill', "./js/index.js"],
+    pug: "./index.pug"
+  },
+
+  output: {
+    filename: "[name].[contenthash].js",
+    path: path.resolve(__dirname, 'dist')
+  },
+
+  resolve: {
+    extensions: ['.js', '.json', '.png', 'pug'],
+  },
+
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: "./index.pug",
+      minify: {
+        collapseWhitespace: isProd
+      }
+    }),
+    new CleanWebpackPlugin(),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, 'src/assets/favicon/favicon.ico'),
+          to: path.resolve(__dirname, 'dist')
+        }
+      ]
+    }),
+    new MiniCssExtractPlugin({
+      filename: "[name].[contenthash].css"
+    })
+  ],
+
+  optimization: optimization(),
+
+  devServer: {
+    port: 4200,
+    hot: isDev,
+  },
+
+  devtool: isDev ? 'source-map' : '',
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: cssLoaders()
+      },
+      {
+        test: /\.scss$/,
+        use: cssLoaders('sass-loader')
+      },
+      {
+        test: /\.(png|jpg|svg|gif)$/,
+        use: ['file-loader']
+      },
+      {
+        test: /\.(ttf|woff|woff2|eot)$/,
+        use: ['file-loader']
+      },
+      {
+        test: /\.js$/ ,
+        exclude : /node_modules/ ,
+        use: jsLoaders(),
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader',
+            options: {
+              minimize: true
+            }
+          }
+        ]
+      },
+      {
+        test: /\.pug$/,
+        loader: 'pug-loader',
+
+      }
+  ]
+}
+}
