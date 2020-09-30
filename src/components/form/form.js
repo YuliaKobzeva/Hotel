3df7f27e167f7b47f13375e5ac5559b1d810c987
Yuliya <lomeyka@gmail.com>
2020-09-30 13:54:30 +0300

    input datepickers are stylizated, tried to connect JS to the button. Nothing happens...(

---
+++ src/components/form/form.js
@@ -0,0 +1,10 @@
+
+const buttonsArrowDown = Array.from(document.getElementsByClassName('datepicker__button-arrow-down'));
+
+for (let i = 0; i < buttonsArrowDown.length; i++) {
+  buttonsArrowDown[i].addEventListener("click", () => {
+    buttonsArrowDown[i].classList.add("datepicker-here")
+  })
+}
+
+console.log(buttonsArrowDown);
